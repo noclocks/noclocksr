6a93b5d1e18876fdfcc9b3d69b55468f041c28d7
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2024-02-07 15:14:37 -0500

    feat: add examples

---
+++ examples/ex-shiny_resume.R
@@ -0,0 +1,72 @@
+library(shiny)
+# library(noclocksR)
+
+ui <- function(request) {
+  shiny_resume_page(
+    app_title = "DEMO",
+    nav = shiny_resume_navbar(
+      refs = c(
+        "about" = "About",
+        "experience" = "Experience",
+        "education" = "Education",
+        "interests" = "Interests",
+        "awards" = "Awards"
+      ),
+      image = "https://github.com/jimbrig/assets/blob/main/images/2020-Headshot-Round%20(Custom).png?raw=true",
+      color = "black"
+    ),
+    body = shiny_resume_body(
+      shiny_resume_section(
+        id = "about",
+        h4("About"),
+        p("About me...")
+      ),
+      shiny_resume_section(
+        id = "experience",
+        h4("Experience"),
+        p("Experience...")
+      ),
+      shiny_resume_section(
+        id = "education",
+        h4("Education"),
+        p("Education...")
+      ),
+      shiny_resume_section(
+        id = "interests",
+        h4("Interests"),
+        p("Interests...")
+      ),
+      shiny_resume_section(
+        id = "awards",
+        h4("Awards"),
+        p("Awards...")
+      )
+    )
+  )
+}
+
+server <- function(input, output, session){
+
+  # output$about <- renderPlot({
+  #   plot(iris)
+  # })
+  #
+  # output$experience <- renderPlot({
+  #   plot(mtcars)
+  # })
+  #
+  # output$education <- renderPlot({
+  #   plot(airquality)
+  # })
+  #
+  # output$interests <- renderPlot({
+  #   plot(AirPassengers)
+  # })
+  #
+  # output$awards <- renderPlot({
+  #   plot(anscombe)
+  # })
+
+}
+
+shinyApp(ui, server)
